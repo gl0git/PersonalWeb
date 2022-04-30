1a8083f508f35cbc2388f4f774eb46feb0c44f14
coG9 <williamcolinsgeorge@gmail.com>
2022-04-30 14:51:35 -0400

    changing some stuffs

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,7 +3,7 @@ export default function Heading(){
         <div className='heading'>
             <div className='logo'>RAW THOUGHT</div>
             <div className='credit'>Inspired by <a href='https://coG9.github.io/PersonalWeb/#/weblog'>Aaron Swartz</a></div>
-            <div className='credit'>Written by <a href='https://coG9.github.io/PersonalWeb/#/weblog'>Micah Omondi</a></div>
+            <div className='credit'>Written by <a href='https://coG9.github.io/PersonalWeb/#/weblog'>Micah</a></div>
         </div>
     )
 }
